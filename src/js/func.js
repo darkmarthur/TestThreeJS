fa55c8bfb2a92200c2aa695bdff45c99ab0e07e6
mdominguez <darkmarthur@gmail.com>
2018-12-30 00:30:06 -0600

    Initial

---
+++ src/js/func.js
@@ -0,0 +1,11 @@
+// const greet = name => {
+//     return `Hello asdasdasda ${name}`;
+// };
+
+// export default greet;
+
+
+// export const repeat = (string) => `${string} ${string}`;
+// export function shout(string) {
+//   return `${string.toUpperCase()}!`;
+// }
